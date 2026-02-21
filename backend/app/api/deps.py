df4b58e1fc9fa5299c17248ee770865380fdf188
M-SAAD-BIN-MAZHAR <msaadbinmazhar@gmail.com>
2026-02-22 02:36:07 +0500

    Fixed minor frontend linting warnings

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -28,7 +28,6 @@ def get_current_user(
     user = db.query(models.User).filter(models.User.id == int(token_data.sub)).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    if not user.is_active:
     return user
 
 def get_current_active_user(
